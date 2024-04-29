18a2113b94449f914878be5ea591dd3a5c44d52d
sp00n8 <53258848+sp00n8@users.noreply.github.com>
2024-04-29 23:38:03 +0300

    main

--- main.py
+++ main.py
@@ -1,13 +1,108 @@
-# 1. Создайте базовый класс `Animal`, который будет содержать общие атрибуты (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
-#
-# 2. Реализуйте наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`, которые наследуют от класса `Animal`. Добавьте специфические атрибуты и переопределите методы, если требуется (например, различный звук для `make_sound()`).
-#
-# 3. Продемонстрируйте полиморфизм: создайте функцию `animal_sound(animals)`, которая принимает список животных и вызывает метод `make_sound()` для каждого животного.
-#
-# 4. Используйте композицию для создания класса `Zoo`, который будет содержать информацию о животных и сотрудниках. Должны быть методы для добавления животных и сотрудников в зоопарк.
-#
-# 5. Создайте классы для сотрудников, например, `ZooKeeper`, `Veterinarian`, которые могут иметь специфические методы (например, `feed_animal()` для `ZooKeeper` и `heal_animal()` для `Veterinarian`).
-#
-# Дополнительно:
-# Попробуйте добавить дополнительные функции в вашу программу, такие как сохранение информации о зоопарке в файл и возможность её загрузки, чтобы у вашего зоопарка было "постоянное состояние" между запусками программы.
+import pickle
+import os
 
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def make_sound(self):
+        raise NotImplementedError("не имплементирован")
+
+    def eat(self):
+        print(f"{self.name} ест.")
+
+class Bird(Animal):
+    def make_sound(self):
+        print(f"{self.name} чирикает!")
+
+class Mammal(Animal):
+    def make_sound(self):
+        print(f"{self.name} хрюкает!")
+
+class Reptile(Animal):
+    def make_sound(self):
+        print(f"{self.name} шипит!")
+
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+class ZooKeeper:
+    def __init__(self, name):
+        self.name = name
+        self.role = "работник"
+
+    def feed_animal(self, animal):
+        print(f"{self.name} кормит {animal.name}.")
+
+class Veterinarian:
+    def __init__(self, name):
+        self.name = name
+        self.role = "Ветеринар"
+
+    def heal_animal(self, animal):
+        print(f"{self.name} лечит {animal.name}.")
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def add_staff(self, staff_member):
+        self.staff.append(staff_member)
+
+    def show_animals(self):
+        for animal in self.animals:
+            print(f"{animal.name}, {animal.age} лет")
+
+    def show_staff(self):
+        for staff in self.staff:
+            print(f"{staff.name}, {staff.role}")
+
+    def save_zoo(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump((self.animals, self.staff), file)
+            print("Зоопарк сохранен.")
+
+    def load_zoo(self, filename):
+        if os.path.exists(filename):
+            try:
+                with open(filename, 'rb') as file:
+                    self.animals, self.staff = pickle.load(file)
+                    print("Зоопарк загружен.")
+            except Exception as e:
+                print("Произошла ошибка при чтении файла:", e)
+        else:
+            print("Зоопарк создается первый раз.")
+
+
+zoo = Zoo()
+zoo.load_zoo("zoo_data.pkl")
+
+staff_k1 = ZooKeeper("Иван")
+staff_v1 = Veterinarian("Коля")
+
+zoo.add_staff(staff_k1)
+zoo.add_staff(staff_v1)
+
+boar = Mammal("хряк", 5)
+sparrow = Bird("воробей", 7)
+varan = Reptile("варан", 8)
+
+zoo.add_animal(sparrow)
+zoo.add_animal(boar)
+zoo.add_animal(varan)
+
+zoo.save_zoo("zoo_data.pkl")
+
+zoo.show_animals()
+zoo.show_staff()
+
+animal_sound(zoo.animals)
+
+staff_k1.feed_animal(boar)
+staff_v1.heal_animal(sparrow)
